d309105c126ac159d9f32d940daef1882b9aed20
Steven French <mr.steven.french@gmail.com>
2025-07-11 11:39:57 +1200

    ECS-Component: GetPropertyTypeinfos & usage
    Change-Id: Ib2aeae01d5d96d31b750aacc75ae95a9e558b081

--- tests/ECS.cpp
+++ tests/ECS.cpp
@@ -16,10 +16,11 @@ struct Entity {
 
 struct Component {
     int id;
-    virtual std::vector<std::string>& GetPropertyList() = 0;
+    virtual int GetPropertyIndexByName(const std::string& prop_name) = 0;
+    virtual const std::vector<std::string>& GetPropertyNames() = 0;
     virtual void* GetVoidPropertyByIndex(int prop_index) = 0;
     virtual void* GetVoidPropertyByName(const std::string& prop_name) = 0;
-    // virtual std::vector<std::typeinfo> GetPropertyTypeinfos() = 0;
+    virtual const std::vector<const std::type_info*>& GetPropertyTypeinfos() = 0;
     template <typename T>
     T& GetPropertyByIndex(int prop_index) {
         return *(T*)GetVoidPropertyByIndex(prop_index);
@@ -51,13 +52,25 @@ struct PositionComponent : Component {
         z = other.z;
         return *this;
     }
-    std::vector<std::string>& GetPropertyList() override {
-        static std::vector<std::string> property_list = {
+    int GetPropertyIndexByName(const std::string& prop_name) override {
+        static std::unordered_map<std::string, int> prop_name_indexes = {
+            {"x", 0},
+            {"y", 1},
+            {"z", 2}
+        };
+        auto it = prop_name_indexes.find(prop_name);
+        if (it == prop_name_indexes.end()) {
+            return -1;
+        }
+        return it->second;
+    }
+    const std::vector<std::string>& GetPropertyNames() override {
+        static std::vector<std::string> prop_names = {
             "x",
             "y",
             "z"
         };
-        return property_list;
+        return prop_names;
     }
     void* GetVoidPropertyByIndex(int prop_index) override {
         switch (prop_index) {
@@ -71,16 +84,19 @@ struct PositionComponent : Component {
         return 0;
     }
     void* GetVoidPropertyByName(const std::string& prop_name) override {
-        static std::unordered_map<std::string, int> prop_name_indexes = {
-            {"x", 0},
-            {"y", 1},
-            {"z", 2}
-        };
-        auto it = prop_name_indexes.find(prop_name);
-        if (it == prop_name_indexes.end()) {
+        auto prop_index = GetPropertyIndexByName(prop_name);
+        if (prop_index == -1) {
             return 0;
         }
-        return GetVoidPropertyByIndex(it->second);
+        return GetVoidPropertyByIndex(prop_index);
+    }
+    const std::vector<const std::type_info*>& GetPropertyTypeinfos() override {
+        static const std::vector<const std::type_info*> typeinfos = {
+            &typeid(float),
+            &typeid(float),
+            &typeid(float)
+        };
+        return typeinfos;
     }
 };
 
@@ -113,17 +129,34 @@ int main() {
     auto e2_id = ecs.AddEntity({0});
 
     auto e1_ptr = ecs.GetEntity(e1_id);
-    PositionComponent* e1_pos_com = 0, *e2_pos_com = 0;
-    if (e1_ptr) {
-        auto& e1 = *e1_ptr;
-        e1_pos_com = &ecs.ConstructComponent<PositionComponent>(e1.id, PositionComponent(1.f, 1.f, 1.f));
-    }
-    auto e2_ptr = ecs.GetEntity(e2_id);
-    if (e2_ptr) {
-        auto& e2 = *e2_ptr;
-        e2_pos_com = &ecs.ConstructComponent<PositionComponent>(e2.id, PositionComponent(2.f, 2.f, 2.f));
+    assert(e1_ptr);
+    auto& e1 = *e1_ptr;
+    auto& e1_position_component = ecs.ConstructComponent<PositionComponent>(e1.id, PositionComponent(1.f, 1.f, 1.f));
+
+    const auto& position_component_typeinfos = e1_position_component.GetPropertyTypeinfos();
+    const auto& position_component_prop_names = e1_position_component.GetPropertyNames();
+    for (auto& prop_name : position_component_prop_names) {
+        auto prop_index = e1_position_component.GetPropertyIndexByName(prop_name);
+        auto type_info = position_component_typeinfos[prop_index];
+        if (*type_info == typeid(float)) {
+            auto& value = e1_position_component.GetPropertyByIndex<float>(prop_index);
+            std::cout << "Property " << prop_name << "<float>(" << value << ")" << std::endl;
+        } else if (*type_info == typeid(int)) {
+            auto& value = e1_position_component.GetPropertyByIndex<int>(prop_index);
+            std::cout << "Property " << prop_name << "<int>(" << value << ")" << std::endl;
+        } else if (*type_info == typeid(std::string)) {
+            auto& value = e1_position_component.GetPropertyByIndex<std::string>(prop_index);
+            std::cout << "Property " << prop_name << "<std::string>(" << value << ")" << std::endl;
+        } else {
+            // Handle unknown type
+            std::cout << "Property " << prop_name << " has unknown type" << std::endl;
+        }
     }
 
+    auto e2_ptr = ecs.GetEntity(e2_id);
+    assert(e2_ptr);
+    auto& e2 = *e2_ptr;
+    auto& e2_position_component = ecs.ConstructComponent<PositionComponent>(e2.id, PositionComponent(2.f, 2.f, 2.f));
 
     while (window_poll_events(window)) {
         window_render(window);
