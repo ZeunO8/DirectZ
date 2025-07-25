fc80fb19df715454b5f3282b2819ce0106578888
Steven French <mr.steven.french@gmail.com>
2025-07-26 10:59:21 +1200

    iOS: target_link_libraries
    Change-Id: Ib6f93550c0c9bc88422fb6d721e36999e71f4eb3

--- Tests.cmake
+++ Tests.cmake
@@ -11,6 +11,15 @@ function(add_dz_test TEST_NAME TEST_SRC)
     target_link_libraries(${TEST_NAME} PRIVATE DirectZ)
     if(ANDROID)
         target_link_libraries(${TEST_NAME} PRIVATE android log)
+    elseif(IOS)
+        target_link_libraries(${TEST_NAME} PRIVATE
+            "-framework UIKit"
+            "-framework Foundation"
+            "-framework QuartzCore"
+            "-framework Metal"
+        )
+    elseif(MACOS)
+        target_link_libraries(${TEST_NAME} PRIVATE "-framework Cocoa" "-framework QuartzCore" "-framework Metal")
     endif()
     target_compile_features(${TEST_NAME} PRIVATE cxx_std_20)
     add_test(NAME ${TEST_NAME} COMMAND $<TARGET_FILE_DIR:${TEST_NAME}>/${TEST_NAME}${TEST_EXT})
