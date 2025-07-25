27798bb47f130d77ba0eaa76e2572c961cd0a101
Steven French <mr.steven.french@gmail.com>
2025-07-25 17:21:33 +1200

    iOS: update vulkan cmake
    Change-Id: I0f45946db4722ea0e83c7eb7f8a860378d828817

--- cmake/modules/FindVulkan.cmake
+++ cmake/modules/FindVulkan.cmake
@@ -491,9 +491,9 @@ if(MoltenVK IN_LIST Vulkan_FIND_COMPONENTS)
     # CMake has a bug in 3.28 that doesn't handle xcframeworks.  Do it by hand for now.
     if(CMAKE_SYSTEM_NAME STREQUAL "iOS")
         if(CMAKE_VERSION VERSION_LESS 3.29)
-            set( _Vulkan_hint_library_search_paths ${Vulkan_Target_SDK}/ios/lib/MoltenVK.xcframework/ios-arm64)
+            set( _Vulkan_hint_library_search_paths ${Vulkan_Target_SDK}/iOS/lib/MoltenVK.xcframework/ios-arm64)
         else ()
-            set( _Vulkan_hint_library_search_paths ${Vulkan_Target_SDK}/ios/lib/)
+            set( _Vulkan_hint_library_search_paths ${Vulkan_Target_SDK}/iOS/lib/)
         endif ()
     endif ()
     find_library(Vulkan_MoltenVK_LIBRARY
