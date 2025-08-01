1bc7d145f955e17069d3b7dc88cdcb9de332b994
Steven French <mr.steven.french@gmail.com>
2025-08-01 12:43:09 +1200

    zlib: update target properties
    Change-Id: Ie1c8aa0fe9ec2004906098f80bda4a687688d75d

--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -3,5 +3,8 @@ FetchContent_Declare(zlib
     GIT_TAG develop
     GIT_SHALLOW TRUE)
 FetchContent_MakeAvailable(zlib)
+set_target_properties(zlibstatic PROPERTIES OUTPUT_NAME zlibstatic)
+set_target_properties(zlibstatic PROPERTIES DEBUG_POSTFIX "d")
+set_target_properties(zlibstatic PROPERTIES RELEASE_POSTFIX "")
 set(ZLIB_FOUND TRUE)
 set(ZLIB_INCLUDE_DIR ${zlib_SOURCE_DIR})
