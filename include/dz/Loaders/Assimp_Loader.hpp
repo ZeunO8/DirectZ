c3f02953e3dc84b0e76b57f78de9beeaae932386
Steven French <mr.steven.french@gmail.com>
2025-08-02 08:59:31 +1200

    Mesh_Entity_Loader now Assimp_Loader. implemented more
    Change-Id: Ia2c87df937d00b1d61af7d63557f37a3062c5e89

---
+++ include/dz/Loaders/Assimp_Loader.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <filesystem>
+#include <memory>
+#include <stdexcept>
+
+namespace dz::loaders {
+    using MeshPair = std::pair<size_t, int>;
+    using AddMeshFunction = std::function<MeshPair(
+        size_t,
+        const std::vector<vec<float, 4>>&,
+        const std::vector<vec<float, 2>>&,
+        const std::vector<vec<float, 4>>&
+    )>;
+    struct Assimp_Info {
+        AddMeshFunction add_mesh_function;
+        std::filesystem::path path;
+        std::shared_ptr<char> bytes;
+        size_t bytes_length = 0;
+    };
+    struct Assimp_Loader {
+        using value_type = Mesh_Pair;
+        using info_type = Assimp_Info;
+        static value_type Load(const info_type& info);
+    };
+}
--- src/Loaders/Mesh_Entity_Loader.cpp
+++
@@ -1,4 +0,0 @@
-#include <dz/Loaders/Mesh_Entity_Loader.hpp>
-#include "../Assimp/Assimp.hpp"
-Image* dz::loaders::Mesh_Entity_Loader::Load(const dz::loaders::Mesh_Entity_Info& info) {
-}
