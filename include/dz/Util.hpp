7a070fddfc374dfd9b30c53e5d8a2153ce99428e
Steven French <mr.steven.french@gmail.com>
2025-08-31 16:31:04 +1200

    [Util] add trim_string helper function
    Change-Id: Ib8db427599b4c988897212074a1b2dd33605f554

--- include/dz/Util.hpp
+++ include/dz/Util.hpp
@@ -384,4 +384,23 @@ namespace dz
         ifst.read(data.data(), real_limit);
         return data;
     }
+
+    
+    inline static std::string trim_string(const std::string &s)
+    {
+        size_t start = 0;
+        size_t end = s.size();
+
+        while (start < end && std::isspace(static_cast<unsigned char>(s[start])))
+        {
+            start++;
+        }
+
+        while (end > start && std::isspace(static_cast<unsigned char>(s[end - 1])))
+        {
+            end--;
+        }
+
+        return s.substr(start, end - start);
+    };
 }
